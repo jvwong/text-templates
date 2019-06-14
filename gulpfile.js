5d49b57ff26cd1105f67127520e9c988770be9d4
jvwong <jvwong@gmail.com>
2019-06-14 11:39:53 -0400

    Addeing template

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,40 @@
 const { src, dest, series } = require('gulp');
+const template = require('gulp-template');
+const clean = require('gulp-clean');
 
+const FACTOID_BASE_URL = 'https://factoid.baderlab.org/';
+const FACTOID_DOCUMENT_PATH = 'document';
 
-const clean = function( cb ) {
-  // body omitted
-  cb();
+const ARTICLE_DATA = [
+  { 
+    journalName, 
+    title, 
+    authors, 
+    trackingId, 
+    contributorName, 
+    contributorEmail, 
+    editorName, 
+    editorEmail
+    
+    corresponding_author_name: 'Jianyuan Luo',
+    corresponding_author_email: 'luojianyuan@bjmu.edu.cn',
+    corresponding_author_address: 'Department of Medical Genetics, Center for Medical Genetics\nPeking University Health Science Center, Beijing 100191, China',
+    document_url: 'https://factoid.baderlab.org/document/88de82c6-3181-4833-9f4e-b801b9e2f0db/726971cc-d47d-4f9f-b55a-78c88c1c733b' 
+  }
+];
+
+const cleanTask = function( cb ) {
+  return src( 'output/**/*.*' )
+    .pipe( clean( { read: false } ) );
+};
+
+const compileTemplatesTask = function( ) {
+  return src( 'templates/email.html' )
+    .pipe( template( ARTICLE_DATA[0] ))
+    .pipe( dest( 'output' ) );
 };
 
 
-const createEmailTask = function() {
-  // place code for your default task here
-  return src( 'templates/email.txt' )
-    .pipe( dest( 'output' ) );
-}
 
-exports.default = series( clean, createEmailTask ); 
+exports.clean = cleanTask;
+exports.default = series( cleanTask, compileTemplatesTask ); 
