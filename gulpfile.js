c8d45b168ce157526db1fb1fb43d1ef365a954d6
jvwong <jvwong@gmail.com>
2019-06-14 12:04:04 -0400

    More fields.

--- gulpfile.js
+++ gulpfile.js
@@ -2,24 +2,22 @@ const { src, dest, series } = require('gulp');
 const template = require('gulp-template');
 const clean = require('gulp-clean');
 
-const FACTOID_BASE_URL = 'https://factoid.baderlab.org/';
-const FACTOID_DOCUMENT_PATH = 'document';
+const APP_BASE_URL = 'https://factoid.baderlab.org/';
+const APP_DOCUMENT_PATH = 'document';
 
 const ARTICLE_DATA = [
   { 
-    journalName, 
-    title, 
-    authors, 
-    trackingId, 
-    contributorName, 
-    contributorEmail, 
-    editorName, 
-    editorEmail
-    
-    corresponding_author_name: 'Jianyuan Luo',
-    corresponding_author_email: 'luojianyuan@bjmu.edu.cn',
-    corresponding_author_address: 'Department of Medical Genetics, Center for Medical Genetics\nPeking University Health Science Center, Beijing 100191, China',
-    document_url: 'https://factoid.baderlab.org/document/88de82c6-3181-4833-9f4e-b801b9e2f0db/726971cc-d47d-4f9f-b55a-78c88c1c733b' 
+    journalName: 'Molecular Cell', 
+    title: 'Acetylation of PHF5A Modulates Stress Responses and Colorectal Carcinogenesis through Alternative Splicing-Mediated Upregulation of KDM3A', 
+    firstAuthor: 'Zhe Wang', 
+    trackingId: 'https://doi.org/10.1016/j.molcel.2019.04.009', 
+    contributorName: 'Jianyuan Luo', 
+    contributorEmail: 'luojianyuan@bjmu.edu.cn', 
+    contributorAddress: 'Department of Medical Genetics, Center for Medical Genetics\nPeking University Health Science Center, Beijing 100191, China',
+    documentUrl: APP_BASE_URL + APP_DOCUMENT_PATH + '/88de82c6-3181-4833-9f4e-b801b9e2f0db/726971cc-d47d-4f9f-b55a-78c88c1c733b',
+    submitByDate: 'July 27, 2019',
+    appUrl: APP_BASE_URL,
+    appName: 'Mentena'
   }
 ];
 
@@ -29,7 +27,7 @@ const cleanTask = function( cb ) {
 };
 
 const compileTemplatesTask = function( ) {
-  return src( 'templates/email.html' )
+  return src( 'templates/email.txt' )
     .pipe( template( ARTICLE_DATA[0] ))
     .pipe( dest( 'output' ) );
 };
