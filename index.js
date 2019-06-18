c9105f7753045f0e320df476d0464915bfa4ccc2
Jeffrey Wong <jvwong@outlook.com>
2019-06-18 09:29:52 -0400

    change name .

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const { renderFromTemplate } = require('./email-template.js');
 
 const data = require('./article-data.js');
 const articleInfo = data[0];
-const templatePath = path.resolve(__dirname, './templates/email.mst' );
+const templatePath = path.resolve(__dirname, './templates/email.txt' );
 const email = renderFromTemplate( templatePath, articleInfo );
 
 console.log(email);
