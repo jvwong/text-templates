2cddb5abb7fc2559eef5dc02cb5c1fa190d18df8
Jeffrey Wong <jvwong@outlook.com>
2019-09-05 10:32:41 -0400

    env variables

--- factoid.js
+++ factoid.js
@@ -1,14 +1,18 @@
+const _ = require('lodash');
 const fetch = require('node-fetch');
+// const mockJson = require('./data/mock-response.json');
 
-const { FACTOID_DOCUMENT_URL } = require('./config.js')
+const { FACTOID_DOC_APIKEY, FACTOID_DOCUMENT_URL } = require('./config.js');
+
 const toJSON = res => res.json();
 const getDocument = article => {
+  const data = _.assign( {}, article, { apiKey: FACTOID_DOC_APIKEY } );
   return fetch( FACTOID_DOCUMENT_URL, {
-        method: 'post',
-        body: JSON.stringify( article ),
-        headers: { 'Content-Type': 'application/json' }
-    })
-    .then( toJSON );
+    method: 'post',
+    body: JSON.stringify( data ),
+    headers: { 'Content-Type': 'application/json' }
+  })
+  .then( toJSON );
 };
 
 module.exports = getDocument;
